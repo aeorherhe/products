5e54d09d33a99fac17e4025292c2b56bb7db81bf
Abraham Orherhe <aeorherhe@icloud.com>
2023-07-20 23:38:16 +0100

    initial commit

---
+++ js/products.js
@@ -0,0 +1,66 @@
+const productContainer = document.querySelector(".single-product-wrap");
+const url = "https://course-api.com/javascript-store-single-product";
+
+const fecthProducts = async () => {
+  productContainer.innerHTML = `<div class="loading-state"></div>`;
+  try {
+    const idSearch = new URLSearchParams(window.location.search);
+    const productID = idSearch.get("id");
+    const response = await fetch(`${url}?id=${productID}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    productContainer.innerHTML = `<div class="section error-stn"><p>There was an error</p></div>`;
+  }
+};
+
+const displayProduct = (product) => {
+  //   color, desc, name, price, img, company
+  const {
+    name,
+    colors,
+    company,
+    description: desc,
+    price,
+    image,
+  } = product.fields;
+  const { url: img } = image[0];
+  document.title = name.toUpperCase();
+
+  const colorsArr = colors
+    .map((eachColor) => {
+      return `<span class="product-color" style="background: ${eachColor}"></span>`;
+    })
+    .join(" ");
+
+  console.log(colorsArr);
+
+  productContainer.innerHTML = `<section class="single-product-ctn">
+            <img
+              src="${img}"
+              alt=""
+              class="product-img"
+            />
+            <div class="product-info-wrap">
+              <h1>${name}</h1>
+              <p>${company}</p>
+              <p class="product-price">$<span class="price">${
+                price / 100
+              }</span></p>
+              <div class="colors-ctn">
+              ${colorsArr}
+              </div>
+              <p class="product-info">
+                ${desc}
+              </p>
+              <button class="btn add-cart">Add to cart</button>
+            </div>
+          </section>`;
+};
+
+const startProducts = async () => {
+  const data = await fecthProducts();
+  displayProduct(data);
+};
+
+startProducts();
---
+++ js/script.js
@@ -0,0 +1,46 @@
+// declare variables
+
+const url = "https://course-api.com/javascript-store-products";
+// const productContainer = document.querySelector(".all-products");
+const productsSection = document.querySelector(".products-stn");
+
+async function fecthProducts() {
+  productsSection.innerHTML = `<div class="loading-state"></div>`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    productsSection.innerHTML = `<div class="section error-stn"><p>There was an error</p></div>`;
+  }
+}
+
+const displayProduct = (allProducts) => {
+  const allProductLists = allProducts
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+
+      return ` <a href="./html/product.html?id=${id}"><div class="product-ctn">
+              <img
+                src="${img}"
+                alt="${title}"
+                class="product-img"
+              />
+              <p class="product-name">${title}</p>
+              <p class="product-price">$<span class="price">${
+                price / 100
+              }</span></p>
+            </div></a>`;
+    })
+    .join(" ");
+  productsSection.innerHTML = `<div class="all-products">${allProductLists}</div>`;
+};
+
+const startProducts = async () => {
+  const data = await fecthProducts();
+  displayProduct(data);
+};
+
+startProducts();
